06bd7bbd3473df7b060c1335e030031929d0dbb4
Antonio Yang <yanganto@gmail.com>
2020-12-31 21:33:50 +0800

    encrypt msg

--- scripts/encrypt.js
+++ scripts/encrypt.js
@@ -1,13 +1,4 @@
-function onResponse(response) {
-  console.log("Received " + response);
-}
-
-function onError(error) {
-  console.log(`Error: ${error}`);
-}
 async function encryptMsg() {
-  let status = document.getElementById("status");
-
   let tab = await browser.tabs.query({
     active: true,
     currentWindow: true,
@@ -15,27 +6,54 @@ async function encryptMsg() {
 
   let details = await browser.compose.getComposeDetails(tab.id);
   let body = details.plainTextBody;
-  let pk = publicKey.value;
-  status.innerHTML = `encrypting with ${pk}... `;
 
-  let encrypted = "encrypted content";
+  let armored_pk = publicKey.value;
+  let pk = (await openpgp.key.readArmored(armored_pk)).keys[0];
+  statusP.innerHTML = `encrypting...`;
 
-  // TODO: figure out how the public key format
-  // const { data: encrypted } = await openpgp.encrypt({
-  //   message: openpgp.message.fromText(body),
-  //   publicKeys: (await openpgp.key.readArmored(pk)).keys[0]
-  // });
-  // console.log(encrypted);
+  const { data: encrypted } = await openpgp.encrypt({
+    message: openpgp.message.fromText(body),
+    publicKeys: pk
+  });
+  browser.compose.setComposeDetails(tab.id, { plainTextBody: encrypted });
 
-  // browser.compose.setComposeDetails(tab.id, { plainTextBody: encrypted });
+  // TODO: sign the encrypted message
+  statusP.innerHTML = `signed message... `;
 
-  // TODO: sign the encrypted message 
-  status.innerHTML = `signed message... `;
   var sending = browser.runtime.sendNativeMessage(
     "trezor_gpg",
     "sign " + encrypted);
   sending.then(onResponse, onError);
-  status.innerHTML = "";
+  statusP.innerHTML = "";
+}
+
+function onResponse(response) {
+  console.log("Received " + response);
+}
+
+function onError(error) {
+  console.log(`Error: ${error}`);
+  statusP.innerHTML = "Please report to https://github.com/yanganto/trezor-gpg/issues " +
+  `Error: ${error}`;
+}
+
+async function signMsg() {
+  let tab = await browser.tabs.query({
+    active: true,
+    currentWindow: true,
+  }).then(tabs => tabs[0]);
+
+  let details = await browser.compose.getComposeDetails(tab.id);
+  let body = details.plainTextBody;
+
+  // TODO: sign the encrypted message
+  statusP.innerHTML = `signed message... `;
+  var sending = browser.runtime.sendNativeMessage(
+    "trezor_gpg",
+    "sign:" + body);
+  sending.then(onResponse, onError);
+  statusP.innerHTML = "";
 }
 
 encryptBTN.addEventListener("click", encryptMsg, false);
+signBTN.addEventListener("click", signMsg, false);
